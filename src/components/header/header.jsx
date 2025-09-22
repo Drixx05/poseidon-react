0ff18ce5915dfa50435cff1cfcfbb3491065ac45
Antonin Grenier <drixion05@gmail.com>
2025-09-22 12:11:32 +0200

    Fixed Folders names

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,7 +3,7 @@ export function Header() {
 		<header>
 			<img
 				src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-				alt="logo mexican guy"
+				alt="Une image de personnage avec un grand sombrero coloré et une guitare"
 			/>
 			<nav>
 				<ul>
