4ab2c9e72676f091e12f432f67cba753cda9ddce
Antonin Grenier <drixion05@gmail.com>
2025-09-22 11:55:30 +0200

    Added Header and Footer components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+        <header>
+            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="logo" />
+            <nav>
+            <a href="/menu">Menu</a>
+            </nav>
+        </header>
+        );
+}
