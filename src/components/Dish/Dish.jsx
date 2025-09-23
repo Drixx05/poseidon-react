b523adef7fbc033c8e15fe12b24b33baa5e9ecae
Antonin Grenier <drixion05@gmail.com>
2025-09-23 11:43:38 +0200

    New Badge on Card

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,13 +1,16 @@
 import Card from "react-bootstrap/Card";
 import "./Dish.scss";
+import Badge from "react-bootstrap/Badge";
 
-export const Dish = ({ name, price, image }) => {
+export const Dish = ({ name, price, image, isNew }) => {
 	return (
 		<Card>
 			<Card.Img variant="top" src={image} alt={name} />
+			{isNew ? <Badge bg="primary">Nouveau</Badge> : null}
 			<Card.Body>
 				<Card.Title>{name}</Card.Title>
 				<Card.Text>{price} €</Card.Text>
+				{isNew ? <Badge bg="primary">Nouveau</Badge> : null}
 			</Card.Body>
 		</Card>
 	);
