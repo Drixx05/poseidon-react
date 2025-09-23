bb3608e83b525aed16c8eeb971526649697d871f
Antonin Grenier <drixion05@gmail.com>
2025-09-23 13:37:47 +0200

    Added Filtered dishes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
 			image:
 				"https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
 			isNew: true,
+			stock: 12
 		},
 		{
 			name: "Enchiladas",
@@ -22,6 +23,7 @@ function App() {
 			image:
 				"https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
 			isNew: false,
+			stock: 0
 		},
 		{
 			name: "Mole poblano",
@@ -29,15 +31,17 @@ function App() {
 			image:
 				"https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
 			isNew: false,
+			stock: 5
 		},
 	];
+	const filteredDishes = dishes.filter((dish) => dish.stock > 0);
 	return (
 		<>
 			<Header />
 			<main>
 				<Container>
 					<Row>
-						{dishes.map((dish, index) => (
+						{filteredDishes.map((dish, index) => (
 							<Col md={4} key={index}>
 								<Dish {...dish} />
 							</Col>
